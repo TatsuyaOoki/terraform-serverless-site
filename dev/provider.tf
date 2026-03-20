0e28266e859abeac59135802de7edf975bb20780
TatsuyaOoki <tat.ooki@outlook.jp>
2026-03-20 15:06:35 +0000

    fix: providerを修正

--- dev/provider.tf
+++ dev/provider.tf
@@ -15,11 +15,11 @@ terraform {
 }
 
 provider "aws" {
-  alias  = "develop"
+  # alias  = "develop"
   region = "ap-northeast-1"
 
-  shared_config_files      = "~/.aws/config"
-  shared_credentials_files = "~/.aws/credentials"
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "terraform"
   default_tags {
     tags = {
