db0acd6c172f7b2b91a0f0cbbadf6aef5bbfc7b2
TatsuyaOoki <tat.ooki@outlook.jp>
2026-03-20 14:50:13 +0000

    first commit

---
+++ dev/provider.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">=1.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  backend "s3" {
+    bucket       = "terraform-state-bucket-0343"
+    key          = "terraform.tfstate"
+    region       = "ap-northeast-1"
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  alias  = "develop"
+  region = "ap-northeast-1"
+
+  shared_config_files      = "~/.aws/config"
+  shared_credentials_files = "~/.aws/credentials"
+  profile                  = "terraform"
+  default_tags {
+    tags = {
+      Environment = local.env
+      CreatedBy   = "terraform"
+    }
+  }
+}
+
