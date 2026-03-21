ad455de59577373adccabf28ad4b85999dbf6ef8
TatsuyaOoki <tat.ooki@outlook.jp>
2026-03-21 06:14:18 +0000

    feat: DynamoDB新規作成

--- dev/data.tf
+++ dev/data.tf
@@ -2,3 +2,21 @@ resource "aws_s3_bucket" "web_bucket" {
   force_destroy = true
   bucket        = "${local.project}-${local.env}-web-bucket-${data.aws_caller_identity.self.account_id}"
 }
+
+# DynamoDB
+resource "aws_dynamodb_table" "album" {
+  name = "${local.project}-${local.env}-album-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "userId"
+  range_key = "photoId"
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+
+  attribute {
+    name = "photoId"
+    type = "S"
+  }
+}
